2d1b030fde2a7f0e72535e5b7edda140bc4228a2
las139 <qqwer153@gmail.com>
2019-01-04 11:43:33 +0900

    d

---
+++ FinalProject-MovieLove/src/spring/vo/bean/ReviewVO.java
@@ -0,0 +1,40 @@
+package spring.vo.bean;
+
+public class ReviewVO {
+	private String REVIEW_MVNAME;
+	private String REVIEW_CONTENTS;
+	private String REVIEW_RATING;
+	private String REVIEW_WRITER;
+	private String REVIEW_DATE;
+	
+	public String getREVIEW_MVNAME() {
+		return REVIEW_MVNAME;
+	}
+	public void setREVIEW_MVNAME(String rEVIEW_MVNAME) {
+		REVIEW_MVNAME = rEVIEW_MVNAME;
+	}
+	public String getREVIEW_CONTENTS() {
+		return REVIEW_CONTENTS;
+	}
+	public void setREVIEW_CONTENTS(String rEVIEW_CONTENTS) {
+		REVIEW_CONTENTS = rEVIEW_CONTENTS;
+	}
+	public String getREVIEW_RATING() {
+		return REVIEW_RATING;
+	}
+	public void setREVIEW_RATING(String rEVIEW_RATING) {
+		REVIEW_RATING = rEVIEW_RATING;
+	}
+	public String getREVIEW_WRITER() {
+		return REVIEW_WRITER;
+	}
+	public void setREVIEW_WRITER(String rEVIEW_WRITER) {
+		REVIEW_WRITER = rEVIEW_WRITER;
+	}
+	public String getREVIEW_DATE() {
+		return REVIEW_DATE;
+	}
+	public void setREVIEW_DATE(String rEVIEW_DATE) {
+		REVIEW_DATE = rEVIEW_DATE;
+	}
+}
